d114015b19f0e6619d7fafbe6d95db9e215aae79
George Mihov <gmihov001@gmail.com>
2021-04-08 22:42:43 +0000

    Revert "Updates"
    This reverts commit f1a0e218252289df7bff4aa24974f419bf518083.

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,37 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => {
-	useEffect(() => {
-		fetch("https://imdb8.p.rapidapi.com/title/get-best-picture-winners", {
-			method: "GET",
-			headers: {
-				"x-rapidapi-key": "da2aafe225mshd2599115ee599ebp1e0ab8jsn5b0724cf5916",
-				"x-rapidapi-host": "imdb8.p.rapidapi.com"
-			}
-		})
-			.then(response => {
-				console.log(response);
-				return response.json();
-			})
-			.then(data => {
-				console.log(data);
-			})
-			.catch(err => {
-				console.error(err);
-			});
-	}, []);
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Top</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button, bootstrap is working
-			</a>
-		</div>
-	);
-};
+export const Home = () => (
+	<div className="text-center mt-5">
+		<h1>Hello Rigo!</h1>
+		<p>
+			<img src={rigoImage} />
+		</p>
+		<a href="#" className="btn btn-success">
+			If you see this green button, bootstrap is working
+		</a>
+	</div>
+);
