6bc4f9ddfa7405a1f558e98dd94ed29eb7b4f394
George Mihov <gmihov001@gmail.com>
2021-04-09 12:51:52 +0000

    Updates

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,7 +6,6 @@ import "../../styles/home.scss";
 export const Home = () => {
 	const [popularMovies, setPopularMovies] = useState([]);
 	const [movieDetails, setMovieDetails] = useState([]);
-	var updatedMovies = [];
 
 	useEffect(() => {
 		fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-boxoffice-movies&page=1", {
@@ -25,14 +24,19 @@ export const Home = () => {
 
 	useEffect(
 		() => {
+			const updatedMovies = [];
+
+			async function fetchData(movie, i) {
+				const response = await fetch("https://www.omdbapi.com/?apikey=aab1d9d2&i=" + movie.imdb_id);
+				const data = await response.json();
+				updatedMovies[i] = data;
+			}
+
 			if (movieDetails.length < 1) {
 				popularMovies.map((movie, ind) => {
-					fetch("https://www.omdbapi.com/?apikey=aab1d9d2&i=" + movie.imdb_id)
-						.then(response => response.json())
-						.then(data => setMovieDetails({ ...movieDetails, [movie.imdb_id]: data }))
-						.catch(err => {
-							console.error(err);
-						});
+					fetchData(movie, ind).then(() => {
+						setMovieDetails([...updatedMovies]);
+					});
 				});
 			}
 		},
