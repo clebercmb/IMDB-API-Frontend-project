c7120567d7a50497cf7593afa4ddb4d6cf34d976
Cleber Miranda Barbosa <cleber@gmail.com>
2021-04-10 14:23:07 -0400

    Fixed fetch calls

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,8 +7,11 @@ export const Home = () => {
 	const [popularMovies, setPopularMovies] = useState([]);
 	const [movieDetails, setMovieDetails] = useState([]);
 
-	useEffect(() => {
-		fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-boxoffice-movies&page=1", {
+	async function fetchMovies() {
+		console.log("First Effect");
+
+		console.log("Fetching...");
+		await fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-recently-added-movies", {
 			method: "GET",
 			headers: {
 				"x-rapidapi-key": "da2aafe225mshd2599115ee599ebp1e0ab8jsn5b0724cf5916",
@@ -18,73 +21,81 @@ export const Home = () => {
 			.then(response => response.json())
 			.then(data => {
 				console.log("data", data);
-				let movieA = {
-					poster: "https://picsum.photos/100/100",
-					key: 1,
-					title: "moviename",
-					year: 2023,
-					boxOffice: "$100",
-					plot: "moviescription"
-				};
-				let movieB = {
-					poster: "https://picsum.photos/100/100",
-					key: 1,
-					title: "moviename",
-					year: 2023,
-					boxOffice: "$100",
-					plot: "moviescription"
-				};
-				setPopularMovies([movieA, movieB]);
+
+				setPopularMovies(data.movie_results);
 				//setPopularMovies(data.movie_results);
 			})
 			.catch(err => {
 				console.error(err);
 			});
-		let movieA = {
-			poster: "https://picsum.photos/100/100",
-			key: 1,
-			title: "moviename",
-			year: 2023,
-			boxOffice: "$100",
-			plot: "moviescription"
-		};
-		let movieB = {
-			poster: "https://picsum.photos/100/100",
-			key: 1,
-			title: "moviename",
-			year: 2023,
-			boxOffice: "$100",
-			plot: "moviescription"
-		};
-		setPopularMovies([movieA, movieB]);
+
+		console.log("End First Effect");
+	}
+
+	useEffect(() => {
+		fetchMovies();
 	}, []);
+
+	async function fetchMoviesDetails() {
+		const updatedMovies = Array(popularMovies.length);
+
+		popularMovies.forEach(async (movie, i) => {
+			await fetch(
+				"https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-images-by-imdb&imdb=" + movie.imdb_id,
+				{
+					method: "GET",
+					headers: {
+						"x-rapidapi-key": "da2aafe225mshd2599115ee599ebp1e0ab8jsn5b0724cf5916",
+						"x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com"
+					}
+				}
+			)
+				.then(response => response.json())
+				.then(data => {
+					updatedMovies[i] = { ...popularMovies[i], ...data, key: i };
+				})
+				.catch(err => {
+					console.error(err);
+				});
+		});
+
+		setMovieDetails(updatedMovies);
+
+		/*     popularMovies.forEach((movie) => {
+      async function fetchData(movie, i) {
+        const response = await fetch(
+          "https://www.omdbapi.com/?apikey=aab1d9d2&i=" + movie.imdb_id
+        );
+        const data = await response.json();
+        updatedMovies[i] = data;
+      }
+
+      if (movieDetails.length < 1) {
+        popularMovies.map((movie, ind) => {
+          fetchData(movie, ind).then(() => {
+            setMovieDetails([...updatedMovies]);
+          });
+        });
+      }
+    });
+ */
+		console.log("Movies=", updatedMovies);
+	}
 
 	useEffect(
 		() => {
-			const updatedMovies = [];
-
-			async function fetchData(movie, i) {
-				const response = await fetch("https://www.omdbapi.com/?apikey=aab1d9d2&i=" + movie.imdb_id);
-				const data = await response.json();
-				updatedMovies[i] = data;
-			}
-
-			if (movieDetails.length < 1) {
-				popularMovies.map((movie, ind) => {
-					fetchData(movie, ind).then(() => {
-						setMovieDetails([...updatedMovies]);
-					});
-				});
-			}
+			fetchMoviesDetails();
 		},
 		[popularMovies]
 	);
+
+	//useEffect(() => fetchMoviesDetails(), [popularMovies]);
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Top Popular Movies in the US</h1>
 			<div className="d-flex flex-wrap">
-				{popularMovies.length > 2
+				{popularMovies.length > 0
 					? popularMovies.map((movie, ind) => {
 							return (
 								<MovieCard
