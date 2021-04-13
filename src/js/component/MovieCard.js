b1a30c5c4404c2bd3b9757ca7fedd61ff5a26249
louisdavidthecoder <louisdavid7070@gmail.com>
2021-04-13 13:38:29 +0000

    idkcommit

--- src/js/component/MovieCard.js
+++ src/js/component/MovieCard.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "../../styles/home.scss";
+import "./MovieCard.scss";
 
 export const MovieCard = props => {
 	return (
-		<div className="text-center mt-5">
-			<div className="card" style={{ width: "18rem" }}>
+		<div className="text-center mt-5 MovieCard-container">
+			<div className="card MovieCard">
 				<img className="card-img-top" src={props.poster} alt="Card image cap" />
 				<div className="card-body">
 					<h5 className="card-title">{props.movie.title}</h5>
