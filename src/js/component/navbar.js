3e2066314e6e34ddca595b447599f7f9886f4f83
George Mihov <gmihov001@gmail.com>
2021-04-15 23:28:29 +0000

    Updates

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -8,8 +8,8 @@ export const Navbar = () => {
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<Link to="/searchmovies">
+					<button className="btn btn-dark btn-lg text-warning">Search movies</button>
 				</Link>
 			</div>
 		</nav>
