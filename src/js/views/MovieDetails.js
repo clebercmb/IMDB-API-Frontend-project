048d7c691c08ffd8ec077e14d888df899a1535d7
George Mihov <gmihov001@gmail.com>
2021-04-16 12:58:20 +0000

    Updates

--- src/js/views/MovieDetails.js
+++ src/js/views/MovieDetails.js
@@ -11,10 +11,16 @@ export const MovieDetails = props => {
 	return (
 		<div className="container MovieDetails-container">
 			Movie Details
-			<div className="card mb-3" style={{ maxWidth: "540px" }}>
+			<div className="card mb-3 bg-dark" style={{ maxWidth: "80vw" }}>
 				<div className="row g-0">
-					<div className="col-md-4" />
-					<div className="col-md-8" />
+					<div className="col-md-4">
+						<img src="" alt="" />
+					</div>
+					<div className="col-md-8">
+						<h2>Title</h2>
+						<p> Year</p>
+						<p> Director </p>
+					</div>
 				</div>
 			</div>
 			<div className="info" style={{ maxWidth: "540px" }}>
